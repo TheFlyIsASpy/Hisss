4816a194e7663a6614e014f919ed047ebe0e8587
TheFlyIsASpy <nicolassherid@gmail.com>
2024-01-09 07:35:44 -0800

    Added error code translations for scanner errors

--- Hisss/MainForm.cs
+++ Hisss/MainForm.cs
@@ -2,6 +2,7 @@
 //This code is licensed under MIT license (see LICENSE.txt for details)
 using System;
 using System.Configuration;
+using System.Resources;
 using System.Windows;
 namespace Hisss
 {
@@ -15,26 +16,31 @@ namespace Hisss
         }
         private void Setup_Scanner(Configuration scanner_config)
         {
+            //Open the scanner connection
             axFiScn1.OpenScanner2(this.Handle.ToInt32());
 
+            //Apply default, commandline, and json config settings
             scanner_config.Apply(axFiScn1);
         }
 
         private void Scan()
         {
             int status;
-            int ErrorCode;
-            //Open the scanner (method)
+
+            //start the scan
             status = axFiScn1.StartScan(this.Handle.ToInt32());
+
             //An error occurred during a scan
             if (status == -1)
             {
-                //Display the error information
-                ErrorCode = axFiScn1.ErrorCode;
-                MessageBox.Show("An error occurred during a scan.\nError code: 0x" + ErrorCode.ToString("X8"), "Exception", MessageBoxButtons.OK, MessageBoxIcon.None, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+                //translate error code from documentation and display
+                ResourceManager rm = new ResourceManager(typeof(ErrorCodes));
+                MessageBox.Show(rm.GetString(axFiScn1.ErrorCode.ToString()) + "\nErrorCode: 0x" + axFiScn1.ErrorCode.ToString("X8"), "Exception", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
             }
+
             //Close the scanner (method)
             axFiScn1.CloseScanner(this.Handle.ToInt32());
         }
+
     }
 }
